0c7cc49612bac67ccdb337d4c80de8c0d0502c7f
only-zover <martinho@alunos.utfpr.edu.br>
2024-06-06 18:33:55 -0300

    change color and font sizes

--- App.js
+++ App.js
@@ -11,8 +11,8 @@ const App = {
             Theme.toggle();
         });
 
-        const arr = ['about', 'about1', 'about2', 'cite'];
-        for (const title of arr) {
+        const titles = ['about', 'about1', 'about2', 'cite'];
+        for (const title of titles) {
             const text = await TypeWriter.getText(title);
             await TypeWriter.write(text, title);
         }
