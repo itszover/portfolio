25a086218528896704556ae9ceb360d832325d70
only-zover <martinho@alunos.utfpr.edu.br>
2024-06-27 14:19:19 -0300

    Refactor file paths for modules in App.js and index.html

--- docs/App.js
+++ docs/App.js
@@ -1,6 +1,6 @@
-import TypeWriter from './TypeWriter.js';
-import Theme from './Theme.js';
-import Project from './Project.js';
+import TypeWriter from '../modules/TypeWriter.js';
+import Theme from '../modules/Theme.js';
+import Project from '../modules/Project.js';
 
 const App = {
     init: async function () {
